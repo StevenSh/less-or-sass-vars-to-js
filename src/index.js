0b90e09451b29678e490db21019d53452b411182
stevensh <scl1203@163.com>
2020-04-20 19:26:17 +0800

    11

--- src/index.js
+++ src/index.js
@@ -1,35 +1,59 @@
-import stripComments from 'strip-json-comments';
+import stripComments from "strip-json-comments";
 
 const varRgx = /^[@$]/;
 const followVar = (value, lessVars, dictionary) => {
   if (varRgx.test(value)) {
     // value is a variable
-    return followVar(lessVars[value] || dictionary[value.replace(varRgx, '')]);
+    return followVar(lessVars[value] || dictionary[value.replace(varRgx, "")]);
   }
   return value;
 };
 
 export default (sheet, options = {}) => {
-  const { dictionary = {}, resolveVariables = false, stripPrefix = false } = options;
+  const {
+    dictionary = {},
+    resolveVariables = false,
+    stripPrefix = false,
+    toLess = false,
+    toSass = false,
+  } = options;
   let lessVars = {};
   const matches = stripComments(sheet).match(/[@$](.*:[^;]*)/g) || [];
 
-  matches.forEach(variable => {
+  matches.forEach((variable) => {
     const definition = variable.split(/:\s*/);
-    let value = definition.splice(1).join(':');
-    value = value.trim().replace(/^["'](.*)["']$/, '$1');
-    lessVars[definition[0].replace(/['"]+/g, '').trim()] = value;
+    let value = definition.splice(1).join(":");
+    value = value.trim().replace(/^["'](.*)["']$/, "$1");
+    lessVars[definition[0].replace(/['"]+/g, "").trim()] = value;
   });
 
   if (resolveVariables) {
-    Object.keys(lessVars).forEach(key => {
+    Object.keys(lessVars).forEach((key) => {
       const value = lessVars[key];
       lessVars[key] = followVar(value, lessVars, dictionary);
     });
   }
 
   if (stripPrefix) {
-    const transformKey = key => key.replace(varRgx, '');
+    const transformKey = (key) => key.replace(varRgx, "");
+
+    lessVars = Object.keys(lessVars).reduce((prev, key) => {
+      prev[transformKey(key)] = lessVars[key];
+      return prev;
+    }, {});
+  }
+
+  if (toLess) {
+    const transformKey = (key) => key.replace(/^[$]/, "@");
+
+    lessVars = Object.keys(lessVars).reduce((prev, key) => {
+      prev[transformKey(key)] = lessVars[key];
+      return prev;
+    }, {});
+  }
+
+  if (toSass) {
+    const transformKey = (key) => key.replace(/^[@]/, "$");
 
     lessVars = Object.keys(lessVars).reduce((prev, key) => {
       prev[transformKey(key)] = lessVars[key];
